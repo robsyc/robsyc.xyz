637f6ad19f0cebf35acf16c79dcf24fa2cf9e0c8
robsyc <robbe.claeys@gmail.com>
2025-03-03 21:14:42 +0100

    2nd attempt at publishing RDF data

--- src/routes/+server.ts
+++ src/routes/+server.ts
@@ -1,16 +1,13 @@
 import type { RequestHandler } from './$types';
+import meData from '$lib/data/me.ttl?raw';
 
-export const GET: RequestHandler = async ({ request, fetch }) => {
+export const GET: RequestHandler = async ({ request }) => {
     const acceptHeader = request.headers.get('Accept') || '';
     
     // Check if the client accepts Turtle format
     if (acceptHeader.includes('text/turtle')) {
-        // Fetch the Turtle file using SvelteKit's fetch
-        const response = await fetch('/src/lib/data/me.ttl');
-        const turtleContent = await response.text();
-        
         // Return the Turtle content with the appropriate content type
-        return new Response(turtleContent, {
+        return new Response(meData, {
             headers: {
                 'Content-Type': 'text/turtle; charset=utf-8'
             }
